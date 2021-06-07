f98773a12e74968f61fb2928d0404a5780577b70
wscnd <wellingson@gmail.com>
2021-06-07 19:58:29 -0300

    [react-typescript] 7.3-advanced component patterns

---
+++ used-projects/17-as-props/src/Application.tsx
@@ -0,0 +1,37 @@
+import * as React from 'react';
+
+type TextOwnProps<E extends React.ElementType = React.ElementType> = {
+  children: string;
+  as?: E;
+};
+
+type TextProps<E extends React.ElementType> = TextOwnProps<E> &
+  Omit<React.ComponentProps<E>, keyof TextOwnProps>;
+
+const defaultElement = 'div';
+
+function Text<E extends React.ElementType = typeof defaultElement>({
+  children,
+  as,
+  ...rest
+}: TextProps<E>) {
+  const TagName = as || defaultElement;
+
+  console.log(rest);
+  return <TagName {...rest}>{children}</TagName>;
+}
+
+const Application = () => {
+  const exampleText =
+    'When I was born, the name for what I was did not exist. They called me nymph, assuming I would be like my mother and aunts and thousand cousins. Least of the lesser goddesses, our powers were so modest they could scarcely ensure our eternities. We spoke to fish and nurtured flowers, coaxed drops from the clouds or salt from the waves. That word, nymph, paced out the length and breadth of our futures.';
+
+  return (
+    <main>
+      <Text as="label" htmlFor="id">
+        {exampleText}
+      </Text>
+    </main>
+  );
+};
+
+export default Application;
